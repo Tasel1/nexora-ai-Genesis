406b88695b2b5846e10042f339083754e2f3b4bf
Tasel <botash860@gmail.com>
2025-11-30 23:05:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def get_openrouter_explanation(shap_values_obj, input_df, predicted_type, target
                 "Content-Type": "application/json"
             },
             data=json.dumps({
-                "model": "x-ai/grok-4.1-fast:free",
+                "model": "tngtech/deepseek-r1t2-chimera:freee",
                 "messages": [{"role": "user", "content": full_prompt}]
             })
         )
